a50019b7dd03a653bef6b59d60d6f7f2553eb190
Roman52 <ser-jar1990@yandex.ru>
2019-06-11 19:08:40 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+	<title>Flex</title>
+</head>
+<body>
+	<!-- <div class="wrapper">
+		<div class="box">
+			<div class="div div_1">1 Lorem ipsum dolor sit amet, consectetur</div>
+			<div class="div div_2">2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas praesentium, ullam, sunt eligendi voluptates, reiciendis quaerat laborum necessitatibus impedit temporibus nihil architecto asperiores accusamus earum dicta totam dolor. Quia, omnis.</div>
+			<div class="div div_3">3Lorem ipsum dolor sit amet, consectetur</div>
+			<div class="div div_4">4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas praesentium, ullam, sunt eligendi voluptates</div>
+		</div>
+	</div> -->
+	<div class="jar">Изменения для Жарова</div>
+	<div class="main">
+		<div class="bl bl_1">
+			<div class="image">
+				<img src="images/cat1.jpg" alt="">
+			</div>
+			<div class="text">
+				<h3>Lorem ipsum dolor.</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+				<a href="#" class="button">Читать</a>
+			</div>
+		</div>
+		<div class="bl bl_2">
+			<div class="image">
+				<img src="images/cat2.jpg" alt="">
+			</div>
+			<div class="text">
+				<h3>Esse, animi, rerum.</h3>
+				<p>Obcaecati animi, eum officiis ut incidunt laboriosam.</p>
+				<a href="#" class="button">Читать</a>
+			</div>
+		</div>
+		<div class="bl bl_3">
+			<div class="image">
+				<img src="images/cat3.jpg" alt="">
+			</div>
+			<div class="text">
+				<h3>Ullam, ad, sunt.</h3>
+				<p>Sint atque sed eaque vero inventore a.</p>
+				<a href="#" class="button">Читать</a>
+			</div>
+		</div>
+		<div class="bl bl_4">
+			<div class="image">
+				<img src="images/cat4.jpg" alt="">
+			</div>
+			<div class="text">
+				<h3>Repellendus, atque, id!</h3>
+				<p>Ipsam tenetur unde quis, eligendi odit illo.</p>
+				<a href="#" class="button">Читать</a>
+			</div>
+		</div>
+		<div class="bl bl_5">
+			<div class="image">
+				<img src="images/cat5.jpg" alt="">
+			</div>
+			<div class="text">
+				<h3>Necessitatibus, architecto, excepturi?</h3>
+				<p>Adipisci magni porro, nisi, suscipit vel temporibus.</p>
+				<a href="#" class="button">Читать</a>
+			</div>
+		</div>
+		<div class="bl bl_6">
+			<div class="image">
+				<img src="images/cat6.jpg" alt="">
+			</div>
+			<div class="text">
+				<h3>Non atque, sequi?</h3>
+				<p>Magni nobis, quae animi dignissimos magnam ut!</p>
+				<a href="#" class="button">Читать</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
